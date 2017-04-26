c01886cc6e2d468ccff0d628b297965c5386c105
Michal Molawka <michal.molawka@sonovum.de>
2017-04-26 13:09:53 +0200

    added 64 bit integer test

--- test/test-message.js
+++ test/test-message.js
@@ -9,4 +9,21 @@ describe('Message', () => {
 		expect(() => new Message(null)).to.throw(Error);
 		expect(() => new Message({})).to.throw(Error);
 	});
+
+	it('should handle 64 bit integers', () => {
+		const message = new Message(Buffer.from([
+			0, 0, 0, 0, 0, 0, 0, 32 + 8, // size
+			0, 0, 0, 0, 0, 0, 0, 1, // id
+			0, 0, 0, 0, 0, 0, 0, 8, // parameter-data-size
+			0, 0, 0, 0, 0, 0, 0, 1, // parameter-id
+			0, 0, 0, 255, 255, 255, 255, 255, // parameter-data
+		]), {
+				messageSizeType   : Message.Types.Int64BE,
+				messageIdType     : Message.Types.Int64BE,
+				parameterSizeType : Message.Types.Int64BE,
+				parameterIdType   : Message.Types.Int64BE,
+		});
+
+		expect(message.getParameter(1).readUInt64BE()).to.equal(0xFFFFFFFFFF);
+	})
 });
